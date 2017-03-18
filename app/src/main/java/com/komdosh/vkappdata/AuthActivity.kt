6a48ae3dbe1ed1a361e94a2bb3f2b21eb255548a
Komdosh <Komdosh@yandex.ru>
2017-03-18 17:50:32 +0300

    Update AuthActivity.kt

--- app/src/main/java/com/komdosh/vkappdata/AuthActivity.kt
+++ app/src/main/java/com/komdosh/vkappdata/AuthActivity.kt
@@ -27,8 +27,6 @@ class AuthActivity : AppCompatActivity() {
         webAuth.clearCache(true)
         webAuth.setWebViewClient(VkWebViewClient())
 
-        webAuth.setWebViewClient(VkWebViewClient())
-
         val url = "https://oauth.vk.com/authorize?client_id=" + Settings.API_VK_ID + "&display=page&redirect_uri=" + URLEncoder.encode(Settings.REDIRECTED_URL, "UTF-8") + "&scope=friends&response_type=token&v=5.62"
         webAuth.loadUrl(url)
         webAuth.visibility = View.VISIBLE
